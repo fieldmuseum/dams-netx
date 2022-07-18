3f0eb9509a9fbd291b306dbca65f96068ea7ac47
magpiedin <magpiedin@gmail.com>
2022-07-17 23:38:12 -0500

    test-add event & catalog rev-attached fields

--- emu_xml_reshape.py
+++ emu_xml_reshape.py
@@ -12,7 +12,7 @@ import utils.xml_tools as xml_tools
 import utils.emu_netx_map as emu_netx
 
 
-def parse_emu_to_dss(emu_record: ET.Element) -> ET.Element:
+def parse_emu_to_dss(emu_record: ET.Element, mm_event: ET.Element, mm_catalog: ET.Element) -> ET.Element:
     '''Parse exported EMu records to DSS-schema records'''
 
     print(emu_record.find('irn').text)
@@ -57,26 +57,52 @@ def parse_emu_to_dss(emu_record: ET.Element) -> ET.Element:
             prepped_record.find(key).text = ref_values
 
 
-    # Populate grouped or ref-table fields with nested tuples (e.g. 'Creator' group )
+    # Populate grouped or ref-table fields with nested tuples (e.g. 'Creator' group, rev-attached Catalog fields)
     group_fields = emu_netx.emu_netx_groups_or_reftabs()
 
     for key, value in group_fields.items():
         if type(value) == list:
-            grouped_value = xml_tools.get_group_value(emu_record, value[0], value[1])
+            if key in ['CatDepartment', 'CatCatalog'] and mm_catalog is not None:
+                grouped_value = xml_tools.get_group_value(mm_catalog, value[0], value[1])
+
+            else:
+                grouped_value = xml_tools.get_group_value(emu_record, value[0], value[1])
+
             prepped_record.find(key).text = grouped_value
+
+
+    # Populate fields where values need concatenation (e.g. reverse-attached Event fields)
+    concat_fields = emu_netx.emu_netx_ref_concatenate()
+
+    for key,value in concat_fields:
+        if type(value) == list:
+            if type(value[1]) == list:
+                ' | '.join(value[1])
+            elif key == 'EveEventURLs' and mm_event is not None:
+                prepped_record.find(key).text = 'https://pj.fieldmuseum.org/event/' + mm_event.find(value[1]).text
+            concat_values = xml_tools.get_group_value(emu_record, value[0], value[1])
+            prepped_record.find(key).text = concat_values
+
 
 
     return prepped_record
     
 
-def main(xml_input, output_emu_prepped=True):
+def main(main_xml_input, event_xml, catalog_xml, output_emu_prepped=True):
     '''Prep input-XML'''
     
     # Load EMu records & fix xml-tags
     emu_tree = ET.ElementTree()
-    emu_records_raw1 = emu_tree.parse(xml_input)
+    emu_records_raw1 = emu_tree.parse(main_xml_input)
     emu_records_raw2 = xml_tools.fix_emu_xml_tags(emu_records_raw1)
     emu_records = xml_tools.convert_linebreaks_to_commas(emu_records_raw2)
+
+    # Import Event & Catalog exports too
+    eve_raw1 = emu_tree.parse(event_xml)
+    cat_raw1 = emu_tree.parse(catalog_xml)
+    
+    eve_records_raw2 = xml_tools.fix_emu_xml_tags(eve_raw1)
+    cat_records_raw2 = xml_tools.fix_emu_xml_tags(cat_raw1)
 
     # Prep DSS output as ET Element -- appendable, similar to a [list]
     dss_records = ET.Element('emultimedia')
@@ -87,8 +113,22 @@ def main(xml_input, output_emu_prepped=True):
 
     # loop through & prep EMu records
     for emu_record in emu_records:
+
+        # grab corresponding Event & Catalog, if any:
+        mm_event = None
+        mm_catalog = None
+
+        for event_record in eve_records_raw2:
+            if event_record.find('AudIdentifier').text == emu_record.find('AudIdentifier').text:
+                mm_event = event_record
+        
+        for cat_record in cat_records_raw2:
+            if cat_record.find('AudIdentifier').text == emu_record.find('AudIdentifier').text:
+                mm_catalog = cat_record
+
+
         # loop thru dss schema fields & populate from EMu xml
-        prepped_record = parse_emu_to_dss(emu_record)
+        prepped_record = parse_emu_to_dss(emu_record, mm_event, mm_catalog)
 
         if prepped_record is not None:
             dss_records.append(prepped_record)
