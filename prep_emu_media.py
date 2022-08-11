5f97a08630936bdf9ee7d77ef2d0223834de1fa2
magpiedin <magpiedin@gmail.com>
2022-08-11 15:09:43 -0500

    troubleshoot / show dept

--- prep_emu_media.py
+++ prep_emu_media.py
@@ -65,15 +65,15 @@ def main():
 
       # Need to grab SecDepartment as well (currently: use only the first value)
       if elem.tag == 'table' and elem.attrib['name'] == 'SecDepartment_tab':
-        sec_dept_tuple_elem = elem.find('tuple')
+        # sec_dept_tuple_elem = elem.find('tuple')
 
         # sec_dept = sec_dept_tuple_elem.findall('atom')
         # # if sec_dept.text is None:
         
-        sec_dept_raw = sec_dept_tuple_elem.findall('tuple/atom')
+        sec_dept_raw = elem.findall('tuple/atom')
         sec_dept_all = []
+        print(sec_dept_raw)
         for dept in sec_dept_raw:
-          print(dept)
           if dept.text is not None:
             print('dept.text =  ' + str(dept.text))
             if len(dept.text) > 0 and dept.text not in sec_dept_all:
