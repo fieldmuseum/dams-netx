322f7872231f8e1ee2b3bc2576eb30e7e6e3071e
magpiedin <magpiedin@gmail.com>
2022-10-21 16:40:07 -0500

    setup xml_tools tests

--- tests/test_utils_xml_tools.py
+++ tests/test_utils_xml_tools.py
@@ -1,14 +1,14 @@
 '''
 Tests for prep_emu_xml.py
 
-python3 -m unittest tests/test_prep_emu_xml.py
+python3 -m unittest tests/test_utils_xml_tools.py
 
 '''
 import glob, unittest, sys
 import xml.etree.ElementTree as ET
 from dotenv import dotenv_values
 
-from utils.xml_tools import fix_emu_xml_tags
+from utils.xml_tools import convert_linebreaks_to_commas, fix_emu_xml_tags
 
 class XMLToolsTestCase(unittest.TestCase):
     def setUp(self) -> None:
@@ -43,14 +43,32 @@ class XMLToolsTestCase(unittest.TestCase):
 
         for child_node in test_xml:
             for emu_column in child_node:
-                print(emu_column.tag)
+                # print(emu_column.tag)
                 self.assertNotEqual("atom", emu_column.tag)
                 self.assertEqual(0, len(emu_column.attrib))
 
 
-    # def test_convert_linebreaks_to_commas(self):
-    #     """"""
+    def test_convert_linebreaks_to_commas(self):
+        """Test if linebreaks are converted to commas"""
 
+        test_xml = fix_emu_xml_tags(self.emu_records_raw)
+        test_xml = convert_linebreaks_to_commas(test_xml)
+
+        for child_node in test_xml:
+            for emu_column in child_node:
+                if str(emu_column.tag) == "DetSubject_tab":
+                    self.assertRegex(emu_column.text, r'^"')
+                    self.assertRegex(emu_column.text, r'"$') #, 'string has ending-quote')
+                    # self.assertIn(r'^"', emu_column.text)
+                    # self.assertIn(r'"$', emu_column.text)
+                    print(emu_column.text)
+
+                    for tuple in emu_column:
+                        print(len(tuple))
+                        for atom in tuple:
+                            print(f'convert -- {atom.text}')
+                # self.assertNotEqual("atom", emu_column.tag)
+                # self.assertEqual(0, len(emu_column.attrib))        
 
     # def test_convert_pipe_to_unique_commas(self):
     #     """"""
