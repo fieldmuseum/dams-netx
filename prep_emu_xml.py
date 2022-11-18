1b0c7efc4d4d2bb4fc13fcb00f65217377ab703c
magpiedin <magpiedin@gmail.com>
2022-11-18 16:46:49 -0600

    start fixing pylint issues

--- prep_emu_xml.py
+++ prep_emu_xml.py
@@ -3,16 +3,23 @@ Parse EMu XML to DAMS-ready XML
 2022-Jun; FMNH, based on EMu-xml-to-json convert_xml.py
 '''
 
+import glob
+import logging
+import os
+import re
+import sys
 import xml.etree.ElementTree as ET
-import glob, logging, os, re, sys
 # from decouple import config
-import utils.dss_schema as dss_schema
-import utils.xml_tools as xml_tools
-import utils.emu_netx_map as emu_netx
-import utils.setup as setup
+from utils import dss_schema
+from utils import xml_tools
+from utils import emu_netx_map as emu_netx
+from utils import setup
 
 
-def parse_emu_to_dss(emu_record: ET.Element, mm_event: ET.Element, mm_catalog: ET.Element) -> ET.Element:
+def parse_emu_to_dss(
+    emu_record: ET.Element,
+    mm_event: ET.Element,
+    mm_catalog: ET.Element) -> ET.Element:
     '''Parse exported EMu records to DSS-schema records'''
 
     print(emu_record.find('irn').text)
@@ -33,39 +40,41 @@ def parse_emu_to_dss(emu_record: ET.Element, mm_event: ET.Element, mm_catalog: E
             prepped_record.find(key).text = emu_record.find(value).text
 
             if key == "AudAssociatedSpecimen" and prepped_record.find(key).text is not None:
-                prepped_record.find(key).text = 'https://db.fieldmuseum.org/' + prepped_record.find(key).text
+                guid = prepped_record.find(key).text
+                prepped_record.find(key).text = f'https://db.fieldmuseum.org/{guid}'
 
 
     # Populate table-fields
     table_fields = emu_netx.emu_netx_tables()
 
     for key, value in table_fields.items():
-        if type(value) == list:
+        if isinstance(value, list):
             print('figure this out')
-            
+
         elif value is not None and emu_record.find(value) is not None:
             # This pulls the concatenated value from convert_linebreaks_to_commas()
             prepped_record.find(key).text = emu_record.find(value).text
-    
+
 
     # Populate single-reference-fields (e.g. DetMediaRightsRef)
     ref_fields = emu_netx.emu_netx_refs()
 
     for key, value in ref_fields.items():
 
-        if type(value) == list:
+        if isinstance(value, list):
             ref_values = xml_tools.get_ref_value(emu_record, value[0], value[1])
             if ref_values is not None: # and re.match(r'^\s+$', ref_values) is None:
                 prepped_record.find(key).text = ref_values
             else:
                 prepped_record.find(key).text = ''
 
-    # Populate grouped or ref-table fields with nested tuples (e.g. 'Creator' group, rev-attached Catalog fields)
+    # Populate grouped or ref-table fields with nested tuples
+    #   (e.g. 'Creator' group, rev-attached Catalog fields)
     group_fields = emu_netx.emu_netx_groups_or_reftabs()
 
     for key, value in group_fields.items():
-        if type(value) == list:
-            
+        if isinstance(value, list):
+
             grouped_value = None
 
             if key in ['CatDepartment', 'CatCatalog'] and mm_catalog is not None:
@@ -85,34 +94,34 @@ def parse_emu_to_dss(emu_record: ET.Element, mm_event: ET.Element, mm_catalog: E
 
     for key,value in concat_fields.items():
 
-        # TODO - loop through for multiple reverse-attachments of one kind (e.g. if >1 rev-attached Event records)
+        # TODO - loop through for multiple reverse-attachments of one kind
+        #   (e.g. if >1 rev-attached Event records)
 
         concat_values = None
-        
-        if type(value) == list:
-            if type(value[1]) == list:
+
+        if isinstance(value, list):
+            if isinstance(value[1], list):
                 if len(mm_event.findall('.//' + value[1][0])) > 0:
                     concat_values_raw = []
 
                     for field_name in value[1]:
                         concat_value = mm_event.find('.//' + field_name).text
-                        if concat_value == None:
+                        if concat_value is None:
                             concat_value = 'NULL, check EMu'
 
                         concat_values_raw.append(concat_value)
 
                     concat_values = ' | '.join(concat_values_raw)
                     prepped_record.find(key).text = concat_values
-                    # print('concat_vals = ' + str(prepped_record.find(key).text))
 
             elif key == 'EveEventURLs':
                 if len(mm_event.findall('.//' + value[1])) > 0:
-                    prepped_record.find(key).text = 'https://pj.fieldmuseum.org/event/' + mm_event.find('.//' + value[1]).text
-                    # print('concat_vals = ' + str(prepped_record.find(key).text))
+                    pj_guid = mm_event.find('.//' + value[1]).text
+                    prepped_record.find(key).text = f'https://pj.fieldmuseum.org/event/{pj_guid}'
 
 
     return prepped_record
-    
+
 
 def main():  # main_xml_input, event_xml, catalog_xml):
     '''Given an input-date, prep XML export from that date'''
@@ -131,11 +140,19 @@ def main():  # main_xml_input, event_xml, catalog_xml):
     # if live_or_test == "LIVE":
     #     full_prefix = config['ORIGIN_PATH_XML']
     #     dest_prefix = config['DESTIN_PATH_XML']
-    # else:  # if live_or_test == "TEST": 
+    # else:  # if live_or_test == "TEST":
     #     full_prefix = config['TEST_ORIGIN_PATH_XML']
     #     dest_prefix = config['TEST_DESTIN_PATH_XML']
-    full_prefix = setup.get_path_from_env(live_or_test, config['ORIGIN_PATH_XML'], config['TEST_ORIGIN_PATH_XML'])
-    dest_prefix = setup.get_path_from_env(live_or_test, config['DESTIN_PATH_XML'], config['TEST_DESTIN_PATH_XML'])
+    full_prefix = setup.get_path_from_env(
+        live_or_test,
+        config['ORIGIN_PATH_XML'],
+        config['TEST_ORIGIN_PATH_XML']
+        )
+    dest_prefix = setup.get_path_from_env(
+        live_or_test,
+        config['DESTIN_PATH_XML'],
+        config['TEST_DESTIN_PATH_XML']
+        )
 
     main_xml_input = full_prefix + 'NetX_emultimedia/' + input_date + '/xml*'
     event_xml = full_prefix + 'NetX_mm_events/' + input_date + '/xml*'
@@ -147,7 +164,10 @@ def main():  # main_xml_input, event_xml, catalog_xml):
 
     # Load EMu records & fix xml-tags
     emu_tree = ET.ElementTree()
-    emu_records_raw1 = emu_tree.parse(glob.glob(main_xml_input)[0])  # TODO - test/try to account for empty input-dir
+
+    # TODO - test/try to account for empty input-dir
+    emu_records_raw1 = emu_tree.parse(glob.glob(main_xml_input)[0])
+
     emu_records_raw2 = xml_tools.fix_emu_xml_tags(emu_records_raw1)
     emu_records = xml_tools.convert_linebreaks_to_commas(emu_records_raw2)
 
@@ -155,7 +175,7 @@ def main():  # main_xml_input, event_xml, catalog_xml):
     # Import Event & Catalog exports too
     eve_raw1 = ET.ElementTree().parse(glob.glob(event_xml)[0])
     cat_raw1 = ET.ElementTree().parse(glob.glob(catalog_xml)[0])
-    
+
     eve_records_raw2 = xml_tools.fix_emu_xml_tags(eve_raw1)
     cat_records_raw2 = xml_tools.fix_emu_xml_tags(cat_raw1)
 
@@ -178,7 +198,7 @@ def main():  # main_xml_input, event_xml, catalog_xml):
                 if event_record.findall('AdmGUIDValue') is not None:
                     mm_event = event_record
                     # print("event = " + str(mm_event.findall('./')))
-        
+
         for cat_record in cat_records_raw2:
             if cat_record.find('AudIdentifier').text == emu_record.find('AudIdentifier').text:
                 mm_catalog = cat_record
@@ -194,13 +214,13 @@ def main():  # main_xml_input, event_xml, catalog_xml):
     # Output Prepped DSS-XML
     print("# of dss_records is :  " + str(len(dss_records)) + " | output to: " + dest_prefix)
     if len(dss_records) > 1:
-        if os.path.exists(dest_prefix) == False:
+        if os.path.exists(dest_prefix) is False:
             os.makedirs(dest_prefix)
-        
-        with open(dest_prefix + "dss_prepped.xml", 'wb') as f:
-            f.write(
+
+        with open(dest_prefix + "dss_prepped.xml", 'wb') as output_file:
+            output_file.write(
                 ET.tostring(
-                    dss_records, 
+                    dss_records,
                     xml_declaration=True,
                     encoding='utf-8',
                     short_empty_elements=True
@@ -208,7 +228,7 @@ def main():  # main_xml_input, event_xml, catalog_xml):
                 )
 
     setup.stop_log_dams_netx()
-    
+
 
     # if output_emu_prepped == True:
 
@@ -222,7 +242,7 @@ def main():  # main_xml_input, event_xml, catalog_xml):
     #     with open(dest_prefix + "emu_prepped.xml", 'wb') as f:
     #         f.write(
     #             ET.tostring(
-    #                 emu_out, 
+    #                 emu_out,
     #                 xml_declaration=True,
     #                 encoding='utf-8',
     #                 short_empty_elements=True
