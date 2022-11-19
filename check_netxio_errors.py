7c3a5661ed20066bcb46639ade2be2c3694c9d8a
magpiedin <magpiedin@gmail.com>
2022-11-19 12:45:29 -0600

    start fixing pylint issues

--- check_netxio_errors.py
+++ check_netxio_errors.py
@@ -1,9 +1,13 @@
 '''Check NetXIO lostandfound and sync.log errors'''
 
-from fileinput import filename
-import hashlib, logging, os, re, shutil
-import utils.setup as setup
-import utils.netx_api as netx_api
+import hashlib
+import logging
+import os
+import re
+import shutil
+# from fileinput import filename
+from utils import setup
+from utils import netx_api
 
 
 def list_files(directory:str):
@@ -37,8 +41,8 @@ def main():
     else:
         netxio_source_dir = config['TEST_NETXIO_LOCAL_SOURCE_DIR']
         netxio_lost_dir = config['TEST_NETXIO_LOSTANDFOUND']
-    
-    lost_and_found_files = list_files(netxio_lost_dir) 
+
+    lost_and_found_files = list_files(netxio_lost_dir)
 
     # check for trailing slash in NetXIO source dir value
     if re.match(r'.*/$', netxio_source_dir) is None:
@@ -53,37 +57,44 @@ def main():
 
         # - by checksum
         # - - get md5 of lost-found-file
-        with open(file_path,'rb') as f:
-            bytes = f.read()
-            file_md5 = hashlib.md5(bytes).hexdigest()
+        with open(file_path,'rb') as media_file:
+            file_bytes = media_file.read()
+            file_md5 = hashlib.md5(file_bytes).hexdigest()
 
-        asset_data_md5 = netx_api.netx_get_asset_by_field(search_field="fileChecksum", search_value=file_md5, netx_test=config['NETX_ENV'])
+        asset_data_md5 = netx_api.netx_get_asset_by_field(
+            search_field="fileChecksum",
+            search_value=file_md5,
+            netx_test=config['NETX_ENV']
+            )
 
         # Get NetX Asset ID by md5
-        # If found, remove 
+        # If found, remove
         if 'result' in asset_data_md5 and len(asset_data_md5['result']['results']) > 0:
 
-            asset_id = asset_data_md5['result']['results'][0]['id']
+            asset_id = f"NetX asset {asset_data_md5['result']['results'][0]['id']}"
 
-            log_message = f'Removed {file_name} from lostandfound - md5 {file_md5} matches NetX asset {asset_id}'
-            print(log_message)
-            logging.info(log_message)
+            log_msg = f'Removed {file_name} from lostandfound: md5 {file_md5} matches {asset_id}'
+            print(log_msg)
+            logging.info(log_msg)
 
             os.remove(file_path)
-        
+
         # If not found, check by filename (next if/else)
-        else:        
+        else:
 
             # Check if file is in NetX
             # - by filename
-            asset_data = netx_api.netx_get_asset_by_filename(file_name=file_name, netx_env=config['NETX_ENV'])
+            asset_data = netx_api.netx_get_asset_by_filename(
+                file_name=file_name,
+                netx_env=config['NETX_ENV']
+                )
 
             if 'result' in asset_data and len(asset_data['result']['results']) > 0:
 
-                asset_id = asset_data['result']['results'][0]['id']
+                asset_id = f"NetX asset {asset_data['result']['results'][0]['id']}"
                 os.remove(file_path)
 
-                log_message = f'Removed {file_name} from lostandfound - identifier matches NetX asset {asset_id}'
+                log_message = f'Removed {file_name} from lostandfound - guid matches {asset_id}'
                 print(log_message)
                 logging.info(log_message)
 
@@ -92,16 +103,17 @@ def main():
 
                 file_path_edit = re.sub(netxio_lost_dir ,'', file_path)
                 file_path_edit = re.sub(file_name ,'', file_path_edit)
+                moving_file = f'file for NetXIO returned to: {netxio_source_dir + file_path_edit}'
 
-                log_message = f'No NetX match for {file_name} (md5 {file_md5}) - moving file for NetXIO back to: {netxio_source_dir + file_path_edit}'
+                log_message = f'No NetX match for {file_name} (md5 {file_md5}) - {moving_file}'
                 print(log_message)
                 logging.info(log_message)
 
 
-                if os.path.exists(netxio_source_dir + file_path_edit) == False:
+                if os.path.exists(netxio_source_dir + file_path_edit) is False:
                     os.makedirs(netxio_source_dir + file_path_edit)
-                
-                if os.path.exists(netxio_source_dir + file_path_edit + file_name) == False:
+
+                if os.path.exists(netxio_source_dir + file_path_edit + file_name) is False:
                     shutil.move(file_path, netxio_source_dir + file_path_edit)
 
             else:
@@ -109,9 +121,9 @@ def main():
                 log_message = 'Possible API error'
                 print(log_message)
                 logging.error(log_message)
-                        
+
     setup.stop_log_dams_netx()
 
 
 if __name__ == '__main__':
-  main()
+    main()
