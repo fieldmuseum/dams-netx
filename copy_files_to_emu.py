c77ff5478ec99dfd463e0f61a19cd7f30ff1b0ec
Kate Webbink <kwebbink@fieldmuseum.org>
2024-12-06 08:54:24 -0600

    add functions to copy files to emu (#55)

---
+++ copy_files_to_emu.py
@@ -0,0 +1,31 @@
+import utils.media_tools as um
+import utils.csv_tools as uc
+
+def main():
+
+    # # Import tables:
+    # dato_media = uc.rows('Dato_media_full_export-2024012003_all.csv')
+    # dato_guides = uc.rows('Dato_full_export-fg_guide-2024-12-04.csv')
+    # emu_guides = uc.rows('EMu_thumbs_group1_prep_import2.csv')
+
+    # # TABLE-1: merge dato_media.dato-pdf names to dato-thumbs by fg_guide id
+    # # TABLE-2: merge Group1_prep.emu irns to TABLE-1 by pdf names
+    # # TABLE-3: from [path + name], to [path + name]
+
+    # # out:
+    # # - create list of
+    # #     - Shackleton file-path (formed from EMu IRN)
+    # #     - from_name (from Dato filenames)
+    # #     - to_name (formed from EMu-PDF filename)
+
+    # Import CSV with 
+    from_to = uc.rows('from_to_thumbs.csv')
+
+    # Copy files from local to server
+    um.copy_files_in_list(paths_list_csv=from_to, 
+                          from_path_prefix='thumb_batch/',
+                          env='TEST')
+
+
+if __name__ == "__main__":
+    main()
