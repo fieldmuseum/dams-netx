14c41edab30b3a24b5f3f28d38a007ae798a73b4
magpiedin <magpiedin@gmail.com>
2024-12-23 16:36:44 -0600

    revert to full flask app

--- netx_to_emu_mq.py
+++ netx_to_emu_mq.py
@@ -41,39 +41,39 @@ def publish_to_rabbitmq(message='',
 @app.route('/webhook', methods=['POST'])
 def handle_webhook():
 
-    # '''Receive JSON payload from NetX'''
-    # print(f"Headers: {request.headers}")
-    # print(f"Body: {request.get_data()}")    
-    # data = request.get_json()
+    '''Receive JSON payload from NetX'''
+    print(f"Headers: {request.headers}")
+    print(f"Body: {request.get_data()}")    
+    data = request.get_json()
 
-    # Mockup for a start
-    # # # # #
-    netx_example = {
-        "event": {
-            "assetId": 101,
-            "systemEventType": "ASSET_CREATE",
-            "eventId": "0c218a59-8d6c-4c20-b5fe-372f75162d13",
-            "eventTime": 1674076946,
-            "userId": 1
-        },
-        "token": "6od7VUynjVQbDgLUdzeNvc5KuRs",
-        "messageTime": 1674076946,
-        "messageId": "78480073-b8d2-4494-85a9-638f5a15e409",
-        "subscriptionId": 201
-        }
-    data = json.dumps(netx_example)
-    # # # # Mockup # # # #
+    # # Mockup for a start
+    # # # # # #
+    # netx_example = {
+    #     "event": {
+    #         "assetId": 101,
+    #         "systemEventType": "ASSET_CREATE",
+    #         "eventId": "0c218a59-8d6c-4c20-b5fe-372f75162d13",
+    #         "eventTime": 1674076946,
+    #         "userId": 1
+    #     },
+    #     "token": "6od7VUynjVQbDgLUdzeNvc5KuRs",
+    #     "messageTime": 1674076946,
+    #     "messageId": "78480073-b8d2-4494-85a9-638f5a15e409",
+    #     "subscriptionId": 201
+    #     }
+    # data = json.dumps(netx_example)
+    # # # # # Mockup # # # #
 
     print(data)
 
-    # if data:
-    # Publish to RabbitMQ
-    publish_to_rabbitmq(json.dumps(data))
-    webhook_msg = {'status':'success','message': 'Message sent to RabbitMQ'}
-    return jsonify(webhook_msg), 200
-    # else:
-    #     webhook_msg = {'status':'error','message': 'Invalid payload'}
-    #     return jsonify(webhook_msg), 400
+    if data:
+        # Publish to RabbitMQ
+        publish_to_rabbitmq(json.dumps(data))
+        webhook_msg = {'status':'success','message': 'Message sent to RabbitMQ'}
+        return jsonify(webhook_msg), 200
+    else:
+        webhook_msg = {'status':'error','message': 'Invalid payload'}
+        return jsonify(webhook_msg), 400
 
     # try:
     #     publish_to_rabbitmq(json.dumps(data))
