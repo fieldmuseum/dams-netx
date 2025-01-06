fa5551906e3fa8b0be8b0525368b0190cefebab8
magpiedin <magpiedin@gmail.com>
2025-01-06 09:30:28 -0600

    switch ssl

--- netx_to_emu_mq.py
+++ netx_to_emu_mq.py
@@ -52,5 +52,5 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0',
             port=config['WEBHOOK_PORT'],
             # debug=True,
-            ssl_context="adhoc"
+            # ssl_context="adhoc"
             )
