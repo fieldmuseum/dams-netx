25273a0c7afce5ca15f780283c6666fb3c1f7ddd
magpiedin <magpiedin@gmail.com>
2025-01-06 09:07:57 -0600

    add rmq_env var

--- netx_to_emu_mq.py
+++ netx_to_emu_mq.py
@@ -7,11 +7,13 @@ import utils.rabbit_tools as ur
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+config = dotenv_values('.env')
+
+@app.route('/webhook', methods=['GET'])
 def handle_webhook():
     '''Receive JSON payload from NetX'''
 
-    print(f"Headers: {request.headers}")
+    print("Headers: {'Content-Type': 'text/plain'}")  #  {request.headers}")
     print(f"Body: {request.get_data()}")
     data = None
 
@@ -26,14 +28,18 @@ def handle_webhook():
 
     if data:
 
+        rmq_env = config['RMQ_ENV']
+
         # Publish to RabbitMQ
         try:
-            ur.publish_to_rabbitmq(json.dumps(data))
+            ur.publish_to_rabbitmq(json.dumps(data), env=rmq_env)
             webhook_msg = {'status':'success','message':'Message sent to RabbitMQ'}
+            print(webhook_msg)
             return jsonify(webhook_msg), 200
 
         except Exception as e:
             app.logger.exception("Failed to publish message: %s", e)
+            print(f"Failed to publish message: {e}")
             return jsonify({'status': 'error', 'message': 'Failed to process webhook'}), 500
 
     else:
@@ -45,5 +51,6 @@ if __name__ == '__main__':
     config = dotenv_values('.env')
     app.run(host='0.0.0.0',
             port=config['WEBHOOK_PORT'],
-            # ssl_context="adhoc"
+            # debug=True,
+            ssl_context="adhoc"
             )
